fce551d652899f4086561830b4795f7468a8bf60
Tomás <80177664+TomasVPerez@users.noreply.github.com>
2021-03-09 01:48:15 -0300

    Add files via upload

---
+++ usuario.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+
+nickname = input("Elegi un nombre de usuario: ")
+
+usuario = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+usuario.connect(("127.0.0.1", 55555))
+
+def recibir():
+    while True:
+            try:
+                mensaje = usuario.recv(1024).decode('ascii') #recibimos mensajes del servidor(q se comporta como usuario)
+                if mensaje == 'NICK':
+                    usuario.send(nickname.encode('ascii')) #le mandamos al servidor el nickname para su funcion recibir
+                    pass
+                else:
+                    print(mensaje) #si no recibimos la palabra clave vemos que quiere el servidor
+            except:
+                print("Ocurrio un error ") 
+                usuario.close()
+                break
+
+def escribir():
+    while True:
+        mensaje = f'{nickname}: {input("")}' #esperamos input de los usuarios constantemente, o mandan mensaje o cierran el chat
+        usuario.send(mensaje.encode('ascii')) #enviamos el mensaje al servidor (lo van a ver todos los usuarios por su funcion controlar)
+
+recibirThread = threading.Thread(target=recibir)
+recibirThread.start()
+
+escribirThread = threading.Thread(target=escribir)
+escribirThread.start()
+
+
+
